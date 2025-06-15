85da06c6d0f8fafbc76eeab05d2e13300eb727e6
Mahmoud Muhammad Sani <sanimahmoud10@gmail.com>
2025-06-15 17:28:00 +0000

    The first Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <footer className="py-6 md:px-8 md:py-0 border-t">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-20 md:flex-row">
           <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built with ❤️ for accessibility. SightReader &copy; {new Date().getFullYear()}
+            Built with ❤️ for accessibility. SightReader From SpaceSoftwares Inc &copy; {new Date().getFullYear()}
           </p>
         </div>
       </footer>
